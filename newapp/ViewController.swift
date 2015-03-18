7d46bf3c9f9403cf6db68dbc3895752928da6c4c
Sagar Poudel <poudels14@gmail.com>
2015-03-18 15:25:07 -0400

    Initial commit

--- newapp/ViewController.swift
+++ newapp/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate{
+    
+    
+    @IBOutlet weak var webpage: UIWebView!
+    let notification = NSNotificationCenter.defaultCenter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        self.view.backgroundColor = UIColor.redColor()
+        
+        var request:NSURLRequest = NSURLRequest (URL: NSURL(string: "http://google.com")!)
+        
+        
+        webpage.loadRequest(request)
+//        webpage.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func webView(webView: UIWebView!, didFailLoadWithError error: NSError!) {
+        println("Webview fail with error \(error)");
+    }
+    
+    func webView(webView: UIWebView!, shouldStartLoadWithRequest request: NSURLRequest!, navigationType: UIWebViewNavigationType) -> Bool {
+        println(request)
+        return true;
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView!) {
+        println("Webview started Loading")
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView!) {
+        println("Webview did finish load")
+    }
+    
 }
 
